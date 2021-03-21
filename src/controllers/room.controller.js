fff0a0efbb8e39b1f3130016ed6595dfa5b1c5ef
alorodlla <alorodlla@alum.us.es>
2021-03-21 19:16:44 +0100

    Añadir estructura base para la creación de rooms

---
+++ src/controllers/room.controller.js
@@ -0,0 +1,127 @@
+const bcrypt = require('bcryptjs')
+const parametros = require('../lib/configParameters')
+const mongoose =  require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+const mongodbConnection = require('../databases/mongodb/repository/mongodbManager');
+const mongodbRoom = require('../databases/mongodb/models/room.model')
+
+// conectar a la bbdd
+var mongoDB = "mongodb://127.0.0.1/studybuddies";
+mongoose.connect(mongoDB);
+
+const create = async function(req, res) {
+    
+    let nErrores = 0;
+    let statusCode = 0;
+    let statusMessage = '';
+
+    let description = "";
+    let university = "";
+    let degree = "";
+    let subject = "";
+    let starting_time = new Date();
+    let ending_time = new Date();
+    let price_per_hour = 0;
+    let is_private = false;
+
+    let domain = "";
+    let options = {
+        
+    };
+
+    let conexionMongodb = {};
+
+    let configuracion = parametros.configuracion();
+
+    if(req.body) {
+        if(req.body.description) {
+            description = req.body.description;
+        }
+        if(req.body.university) {
+            university = req.body.university;
+        }
+        if(req.body.degree) {
+            degree = req.body.degree;
+        }
+        if(req.body.subject) {
+            subject = req.body.subject;
+        }
+        if(req.body.starting_time) {
+            starting_time = req.body.starting_time;
+        }
+        if(req.body.ending_time) {
+            ending_time = req.body.ending_time;
+        }
+        if(req.body.price_per_hour) {
+            price_per_hour = req.body.price_per_hour;
+        }
+        if(req.body.is_private) {
+            is_private = req.body.is_private;
+        }
+
+    }
+
+    // Los datos vienen comprobados del front??
+
+    // Generar room db
+    const id = uuidv4();
+
+    const roomBody = {
+        guid_room : id,
+        description: description,
+        university : university,
+        degree : degree,
+        subject : subject,
+        starting_time : starting_time,
+        ending_time : ending_time,
+        price_per_hour : price_per_hour,
+        is_private : is_private,
+        domain : "meet.jit.si",
+        options : {
+            roomName : "studybuddies-"+id,
+            width : 700,
+            height: 700,
+            parentNode: "document.querySelector('#meet')"
+        }
+    };
+
+    //creo la conexion a la base de datos mongodb
+    if (nErrores == 0) {
+        try {
+            conexionMongodb = await mongodbConnection.crearConexion(configuracion.mongoConf.username, configuracion.mongoConf.password, configuracion.mongoConf.name);
+        } catch (err) {
+            console.log('Error al crear la conexion con mongodb. ' + err);
+            statusCode = 500;
+            statusMessage = 'Connection error';
+            nErrores++;
+        }
+    }
+
+    // Crear room en BBDD
+    const room = await new mongodbRoom.room(roomBody);
+    room.save();
+
+    // Cerramos la conexion mongo
+    if (nErrores == 0) 
+    {
+        await mongodbConnection.cerrarConexion(conexionMongodb);
+    }
+
+    // Devolvemos la respuesta
+    if (nErrores == 0) {
+        console.log(`Room creada correctamente`)
+        res.status(200)
+            .json({
+                roomBody
+            });
+    } else {
+        console.log(statusMessage);
+        res.status(statusCode || 500).send(statusMessage || 'General Error');
+    }
+
+}
+
+module.exports = {
+    create
+}
---
+++ src/databases/mongodb/models/rooms.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const RoomSchema = new Schema({
+    guid_room : String,
+    description : String,
+    university : String,
+    degree : String,
+    subject : String,
+    starting_time : Date,
+    ending_time : Date,
+    price_per_hour : Number,
+    is_private : {
+        type : Boolean,
+        default : false
+    },    
+    domain : String,
+    options : {
+        roomName : String,
+        width : Number,
+        height: Number,
+        parentNode: String
+    }
+});
+
+const room = mongoose.model("rooms", RoomSchema);
+
+const getRooms = function (db, dbName) {
+    return new Promise((resolve, reject) => {
+        try {
+            const roomsCollection = db.db(dbName).collection('rooms')
+            const document = roomsCollection.find().toArray(function (err, result) {
+                if (err) reject(err)
+
+                resolve(result)
+            })
+        } catch (err) {
+            reject(err)
+        }
+    })
+}
+
+module.exports = {
+    room,
+    getRooms
+}
