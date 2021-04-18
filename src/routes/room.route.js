4567a5e9a7cc4e45fc582d1212308843f5d79a9e
alejandromol98 <alejandromol98@gmail.com>
2021-04-18 15:18:25 +0200

    Metodo de añadir autprizados cambiado a post

--- src/routes/room.route.js
+++ src/routes/room.route.js
@@ -6,7 +6,7 @@ router.get('/all', roomController.getAll);
 router.get('/:guid', roomController.getById);
 router.get('/student/all', roomController.getSalasEstudioActivas);
 router.get('/tutor/all', roomController.getTutoriasActivas);
-router.put('/autorizar', roomController.anadirAutorizados);
+router.post('/autorizar', roomController.anadirAutorizados);
 router.get('/student/:guid', roomController.getSalasEstudioActivasById);
 router.get('/tutor/:guid', roomController.getTutoriasActivasById);
 
