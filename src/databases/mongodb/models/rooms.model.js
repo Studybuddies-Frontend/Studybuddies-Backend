c703f7652f735fb40697b4451b444de066e366b7
migsaagom <migsaagom@alum.us.es>
2021-04-18 13:03:05 +0200

    Actualización de la lista de autorizados

--- src/databases/mongodb/models/rooms.model.js
+++ src/databases/mongodb/models/rooms.model.js
@@ -135,11 +135,76 @@ const getRoomById = function (db, fechaActual, guid) {
 
 }
 
+const updateRoom = function (db, idRoom, listaAutorizados, colRooms) {
+    return new Promise((resolve, reject) => {
+        try {
+            const roomCollection = db.db("studybuddies").collection(colRooms)
+            roomCollection.updateOne( { "guid" : idRoom },
+            { $set: { "authorised_users" : listaAutorizados } }).then((result) => {
+                resolve()
+            })
+                .catch((err) => {
+                    reject(err)
+                })
+
+        }
+        catch (err) {
+            reject(err)
+        }
+    })
+}
+
+const getSalasEstudioActivasById = function (db, fechaActual, guid) {
+    return new Promise((resolve, reject) => {
+        try {
+            const roomCollection = db.db('studybuddies').collection('rooms');
+            const query = { is_private: false, guid: guid}
+            //if (fechaActual){
+            //	query.fecha_fin = { $gte: fechaActual };
+            //}
+
+
+            const document = roomCollection.find(query).toArray(function (err, result) {
+                if (err) {
+                    reject(err)
+                }
+                resolve(result);
+            });
+        } catch (err) {
+            reject(err);
+        }
+    }
+    )
+}
+
+const getTutoriasActivasById = function (db, fechaActual, guid) {
+    return new Promise((resolve, reject) => {
+        try {
+            const roomCollection = db.db('studybuddies').collection('rooms');
+            const query = { is_private: true, guid: guid}
+            //if (fechaActual){
+            //	query.fecha_fin = { $gte: fechaActual };
+            //}
+            const document = roomCollection.find(query).toArray(function (err, result) {
+                if (err) {
+                    reject(err)
+                }
+                resolve(result);
+            });
+        } catch (err) {
+            reject(err);
+        }
+    })
+}
+
 module.exports = {
     Room,
     guardarRoom,
     getRooms,
     getRoomById,
     getSalasEstudioActivas,
-    getTutoriasActivas
+    getTutoriasActivas,
+    updateRoom,
+    getSalasEstudioActivasById,
+    getTutoriasActivasById
 }
--- src/routes/room.route.js
+++ src/routes/room.route.js
@@ -4,8 +4,11 @@ const roomController = require('../controllers/room.controller')
 router.post('/create', roomController.createRoom);
 router.get('/all', roomController.getAll);
 router.get('/:guid', roomController.getById);
-router.get('/student/all', roomController.getSalasEstudioActivas)
-router.get('/tutor/all', roomController.getTutoriasActivas)
+router.get('/student/all', roomController.getSalasEstudioActivas);
+router.get('/tutor/all', roomController.getTutoriasActivas);
+router.put('/autorizar', roomController.anadirAutorizados);
+router.get('/student/:guid', roomController.getSalasEstudioActivasById);
+router.get('/tutor/:guid', roomController.getTutoriasActivasById);
 
 
 module.exports = router;
